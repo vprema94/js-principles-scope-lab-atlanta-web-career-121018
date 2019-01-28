5dac9d00fddc3851369237d152899e5dfa83acfa
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-01-27 21:06:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+customerName = "bob";
+
+function upperCaseCustomerName() {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "van"
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "asu"
+}
